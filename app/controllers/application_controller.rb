97ec57f6f7c04962009eecffcb134e139be89caa
AustynT <Austyntravis9@gmail.com>
2017-01-19 14:55:12 -0700

    added guest user for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception 
   include DeviseWhitelist
   include SetSource
+  def current_user
+    super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: 'User', email: "guest@example.com")
+  end
+  
 
-  before_action :set_source
 
 
 
-    def set_source
-      session[:source] = params[:q] if params[:q]
-    end
 end
